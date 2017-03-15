0e9e686a198897b9b943572899010c3182f38296
saj0vie <jeffsajoviejr.@Jeffs-iMac-2.local>
2017-03-15 10:48:40 -0400

    Update

--- packages.sh
+++ packages.sh
@@ -3,4 +3,4 @@ dpkg-deb -bZgzip projects/Solstice10 debs
 dpkg-deb -bZgzip projects/IOS6Theme debs
 dpkg-deb -bZgzip projects/Solstice10Circular debs
 dpkg-deb -bZgzip projects/MacOSSierra debs
-dpkg-deb -bZgzip projects/ClassicDock debs
+
