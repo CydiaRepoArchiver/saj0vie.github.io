4fbcdd98ee23b94ce3d277f4412e5fff43f359c9
saj0vie <jdsajovie@gmail.com>
2017-06-05 18:17:02 -0400

    Fix

--- packages.sh
+++ packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/IOS11CellularBars debs
+dpkg-deb -bZgzip projects/IOS11CellularBarsLibstatus9 debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
 
